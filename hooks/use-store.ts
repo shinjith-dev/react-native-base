0d548c4b8b1299b5d88e13b3978416f4e0db1327
shinjith-dev <shinjithpallikkara@gmail.com>
2024-09-03 17:04:25 +0530

    feat: add zustand persist w/ asyncstorage
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- hooks/use-store.ts
+++ hooks/use-store.ts
@@ -1,11 +1,14 @@
 import { create } from 'zustand'
+import {createJSONStorage, persist} from 'zustand/middleware'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface BearState {
   bears: number
   increase: () => void
 }
 
-export const useStore = create<BearState>()((set) => ({
+export const useStore = create<BearState>()(persist((set) => ({
   bears: 0,
   increase: () => set((state) => ({ bears: state.bears + 1 })),
-}))
+}),{name:'bear',
+        storage: createJSONStorage(() => AsyncStorage),}))
